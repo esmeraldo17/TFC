6f8e133320043aa25aa43c98b0bf1461bc437a57
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-03-06 16:29:41 +0100

    feat: testes para FindById e FindAll criados

---
+++ app/backend/src/tests/team.test.ts
@@ -0,0 +1,71 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Team from '../database/models/Team';
+import { Model } from 'sequelize';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testando a rota /teams', () => {
+  /**
+   * Exemplo do uso de stubs com tipos
+   */
+
+  // let chaiHttpResponse: Response;
+
+  // before(async () => {
+  //   sinon
+  //     .stub(Example, "findOne")
+  //     .resolves({
+  //       ...<Seu mock>
+  //     } as Example);
+  // });
+
+  // after(()=>{
+  //   (Example.findOne as sinon.SinonStub).restore();
+  // })
+
+  // it('...', async () => {
+  //   chaiHttpResponse = await chai
+  //      .request(app)
+  //      ...
+
+  //   expect(...)
+  // });
+
+  const TeamList = [
+    {
+      id: 1,
+      teamName: 'Vasco' 
+    },
+    {
+      id: 2,
+      teamName: 'Palmeiras' 
+    }
+  ]
+
+  it('Testa FindAll', async () => {
+    sinon.stub(Model, 'findAll').resolves(TeamList as Team[])
+
+    const result = await chai.request(app).get('/teams');
+
+    expect(result.status).to.deep.equal(200);
+    expect(result.body).to.deep.equal(TeamList);
+  });
+
+  it('Testa FindById', async () => {
+    sinon.stub(Model, 'findByPk').resolves(TeamList as unknown as Team)
+
+    const result = await chai.request(app).get('/teams/1');
+
+    expect(result.status).to.deep.equal(200);
+    expect(result.body).to.deep.equal({id: 1, teamname: 'Vasco'});
+  });
+});
