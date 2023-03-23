301f73ca334b21fbd75383aca9bb947383a60b79
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-03-23 20:39:41 +0100

    feat: mogrations e model de matches criada

---
+++ app/backend/src/database/models/Matches.ts
@@ -0,0 +1,63 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import Team from './Team';
+
+class Matches extends Model {
+  declare id: number;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Matches.init({
+  id: {
+    type: INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  homeTeamId: {
+    type: INTEGER,
+    allowNull: false,
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: INTEGER,
+    allowNull: false,
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  tableName: 'matches',
+  timestamps: false,
+});
+
+Matches.belongsTo(Team, { foreignKey: 'homeTeamId', as: 'homeTeam' });
+Matches.belongsTo(Team, { foreignKey: 'awayTeamId', as: 'awayTeam' });
+
+export default Matches;
