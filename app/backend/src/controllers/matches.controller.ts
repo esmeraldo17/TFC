24495ffbd4451ef69685baa42c86958bdeebe9de
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-03-23 22:11:17 +0100

    fix: adicionando include no findAll

--- app/backend/src/controllers/matches.controller.ts
+++ app/backend/src/controllers/matches.controller.ts
@@ -4,7 +4,9 @@ import MatchesService from '../services/matches.service';
 export default class MatchesController {
   constructor(private matchesService = new MatchesService()) {}
 
-  public getAll = async (_req: Request, res: Response) => {
+  public getAll = async (req: Request, res: Response) => {
+    const inProgress = req.query;
+    console.log(inProgress);
     const matches = await this.matchesService.getAll();
 
     res.status(200).json(matches);
--- app/backend/src/services/matches.service.ts
+++ app/backend/src/services/matches.service.ts
@@ -1,11 +1,17 @@
 import { ModelStatic } from 'sequelize';
 import Matches from '../database/models/Matches';
+import Team from '../database/models/Team';
 
 export default class MatchesService {
   private model: ModelStatic<Matches> = Matches;
 
   public async getAll(): Promise<Matches[]> {
-    const result = await this.model.findAll();
+    const result = await this.model.findAll({
+      include: [
+        { model: Team, as: 'homeTeam', attributes: ['teamName'] },
+        { model: Team, as: 'awayTeam', attributes: ['teamName'] },
+      ],
+    });
     return result;
   }
 }
